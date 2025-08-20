2286fc3d21d511f13e9d017e5fca700c8d531aca
Idelia Muthia <ideliamuthia@gmail.com>
2025-08-20 10:25:31 +0700

    datatable pasien

--- src/components/ecommerce/RecentOrders.tsx
+++ src/components/ecommerce/RecentOrders.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Table,
   TableBody,
@@ -7,6 +9,8 @@ import {
 } from "../ui/table";
 import Badge from "../ui/badge/Badge";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
 // Define the TypeScript interface for the table rows
 interface Product {
@@ -20,62 +24,37 @@ interface Product {
   status: "Delivered" | "Pending" | "Canceled"; // Status of the product
 }
 
-// Define the table data using the interface
-const tableData: Product[] = [
-  {
-    id: 1,
-    name: "MacBook Pro 13”",
-    variants: "2 Variants",
-    category: "Laptop",
-    price: "$2399.00",
-    status: "Delivered",
-    image: "/images/product/product-01.jpg", // Replace with actual image URL
-  },
-  {
-    id: 2,
-    name: "Apple Watch Ultra",
-    variants: "1 Variant",
-    category: "Watch",
-    price: "$879.00",
-    status: "Pending",
-    image: "/images/product/product-02.jpg", // Replace with actual image URL
-  },
-  {
-    id: 3,
-    name: "iPhone 15 Pro Max",
-    variants: "2 Variants",
-    category: "SmartPhone",
-    price: "$1869.00",
-    status: "Delivered",
-    image: "/images/product/product-03.jpg", // Replace with actual image URL
-  },
-  {
-    id: 4,
-    name: "iPad Pro 3rd Gen",
-    variants: "2 Variants",
-    category: "Electronics",
-    price: "$1699.00",
-    status: "Canceled",
-    image: "/images/product/product-04.jpg", // Replace with actual image URL
-  },
-  {
-    id: 5,
-    name: "AirPods Pro 2nd Gen",
-    variants: "1 Variant",
-    category: "Accessories",
-    price: "$240.00",
-    status: "Delivered",
-    image: "/images/product/product-05.jpg", // Replace with actual image URL
-  },
-];
+interface Pasien {
+  id: number;
+  no_rekam_medis: string;
+  nama: string;
+  tgl_masuk: string;
+  tgl_lahir: string;
+  dpjp: string;
+  ppja: string;
+  kamar: string;
+  status: "draft" | "completed";
+  master_ruangan: {nama: string;}
+}
 
 export default function RecentOrders() {
+  const [pasien, setPasien] = useState<Pasien[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/pasien");
+      const data = await res.json();
+      setPasien(data);
+    };
+    fetchData();
+  }, []);
+  
   return (
     <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
       <div className="flex flex-col gap-2 mb-4 sm:flex-row sm:items-center sm:justify-between">
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
-            Recent Orders
+            Daftar Pasien
           </h3>
         </div>
 
@@ -130,25 +109,49 @@ export default function RecentOrders() {
             <TableRow>
               <TableCell
                 isHeader
-                className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                className="py-3 font-medium text-gray-500 text-center text-theme-lg dark:text-gray-400"
               >
-                Products
+                Nomor Rekam Medis
               </TableCell>
               <TableCell
                 isHeader
-                className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                className="py-3 font-medium text-gray-500 text-center text-theme-lg dark:text-gray-400"
               >
-                Category
+                Nama
               </TableCell>
               <TableCell
                 isHeader
-                className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                className="py-3 font-medium text-gray-500 text-center text-theme-lg dark:text-gray-400"
               >
-                Price
+                Tanggal Masuk
               </TableCell>
               <TableCell
                 isHeader
-                className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
+                className="py-3 font-medium text-gray-500 text-center text-theme-lg dark:text-gray-400"
+              >
+                Tanggal Lahir
+              </TableCell>
+              <TableCell
+                isHeader
+                className="py-3 font-medium text-gray-500 text-center text-theme-lg dark:text-gray-400"
+              >
+                Nama DPJP
+              </TableCell>
+              <TableCell
+                isHeader
+                className="py-3 font-medium text-gray-500 text-center text-theme-lg dark:text-gray-400"
+              >
+                Nama PPJA
+              </TableCell>
+              <TableCell
+                isHeader
+                className="py-3 font-medium text-gray-500 text-center text-theme-lg dark:text-gray-400"
+              >
+                Kamar
+              </TableCell>
+              <TableCell
+                isHeader
+                className="py-3 font-medium text-gray-500 text-center text-theme-lg dark:text-gray-400"
               >
                 Status
               </TableCell>
@@ -157,48 +160,32 @@ export default function RecentOrders() {
 
           {/* Table Body */}
 
-          <TableBody className="divide-y divide-gray-100 dark:divide-gray-800">
-            {tableData.map((product) => (
-              <TableRow key={product.id} className="">
-                <TableCell className="py-3">
-                  <div className="flex items-center gap-3">
-                    <div className="h-[50px] w-[50px] overflow-hidden rounded-md">
-                      <Image
-                        width={50}
-                        height={50}
-                        src={product.image}
-                        className="h-[50px] w-[50px]"
-                        alt={product.name}
-                      />
-                    </div>
-                    <div>
-                      <p className="font-medium text-gray-800 text-theme-sm dark:text-white/90">
-                        {product.name}
-                      </p>
-                      <span className="text-gray-500 text-theme-xs dark:text-gray-400">
-                        {product.variants}
-                      </span>
-                    </div>
-                  </div>
+          <TableBody>
+            {pasien.map((p) => (
+              <TableRow key={p.id}>
+                <TableCell>{p.no_rekam_medis}</TableCell>
+                <TableCell>{p.nama}</TableCell>
+                <TableCell>
+                  {moment(new Date(p.tgl_masuk)).format("DD-MM-YYYY HH:mm")}
                 </TableCell>
-                <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-                  {product.price}
+                <TableCell>
+                  {moment(new Date(p.tgl_lahir)).format("DD-MM-YYYY")}
                 </TableCell>
-                <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-                  {product.category}
-                </TableCell>
-                <TableCell className="py-3 text-gray-500 text-theme-sm dark:text-gray-400">
+                <TableCell>{p.dpjp}</TableCell>
+                <TableCell>{p.ppja}</TableCell>
+                <TableCell>{p.master_ruangan.nama}</TableCell>
+                <TableCell>
                   <Badge
                     size="sm"
                     color={
-                      product.status === "Delivered"
-                        ? "success"
-                        : product.status === "Pending"
+                      p.status === "draft"
                         ? "warning"
+                        : p.status === "completed"
+                        ? "success"
                         : "error"
                     }
                   >
-                    {product.status}
+                    {p.status}
                   </Badge>
                 </TableCell>
               </TableRow>
